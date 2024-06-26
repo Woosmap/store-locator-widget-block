d282724452f1f938f6830cb90b2f9370b6fa90fc
Camilo <camilo_hd@yahoo.com>
2024-06-26 13:39:35 +0200

    fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '53dda70b65590b728dd1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5b4abd1cb21b3688239e');
