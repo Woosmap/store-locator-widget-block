d1e61294830abd4ae1685517d124cab3f23b3594
galela <gsimon@woosmap.com>
2024-07-03 16:45:55 +0200

    chore: removed console log

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '200a824d2bd518362a15');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '656d355e0e021b702dd4');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-dom-ready'), 'version' => '450e1f16948e31616932');
+<?php return array('dependencies' => array('wp-data', 'wp-dom-ready'), 'version' => '9051a20c0a3f8377b9a2');
