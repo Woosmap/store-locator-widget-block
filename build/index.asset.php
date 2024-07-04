d3ab36829da7c2a187b053fe483afee4d6610f34
galela <gsimon@woosmap.com>
2024-07-04 16:21:01 +0200

    chore: updated build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aaa3bf63b5fee42006de');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fde49b46c0a95c508c65');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-dom-ready'), 'version' => '429936b50e24777b237a');
+<?php return array('dependencies' => array('wp-data', 'wp-dom-ready'), 'version' => '3b86338e8ef6e9816709');
